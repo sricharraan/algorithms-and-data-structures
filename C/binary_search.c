9cf16e9160277526b4f0a3ac19c0ea90aa992f33
Harsh Agrawal <harsh.agrawal17@siesgst.ac.in>
2018-10-13 21:31:10 +0530

    Changed Binary search file

--- C/binary_search.c
+++ C/binary_search.c
@@ -10,22 +10,6 @@ typedef struct Obj{
 returns the array indice
 key must be ordered
 */
-int BinSearch(Obj vec[], int key){
-	int end=Maxtam-1,begin = 0, mid = (Maxtam-1)/2;
-	while(begin<end && key != vec[mid].key){
-		if(key > vec[mid].key)
-		begin = mid+1;
-		else
-		end = mid-1;
-		
-		mid = (begin+end)/2;
-	}
-	if(key == vec[mid].key)
-	return mid;
-	
-	return -1;
-}
-
 main(){
 	Obj vec[Maxtam];
 	Obj aux;
@@ -45,3 +29,21 @@ main(){
 	printf("Not Found");
 	
 }
+
+int BinSearch(Obj vec[], int key){
+	int end=Maxtam-1,begin = 0, mid = (Maxtam-1)/2;
+	while(begin<end && key != vec[mid].key){
+		if(key > vec[mid].key)
+		begin = mid+1;
+		else
+		end = mid-1;
+		
+		mid = (begin+end)/2;
+	}
+	if(key == vec[mid].key)
+	return mid;
+	
+	return -1;
+}
+
+
